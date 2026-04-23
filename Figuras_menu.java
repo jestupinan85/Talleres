5f6d83d90ed582364b56c9ce6fe1ac1a685cc3a3
jestupinan85 <jestupinan85@unab.edu.co>
2026-04-23 09:46:21 -0500

    Add files via upload

---
+++ Figuras_menu.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Figuras_menu{
+    static Scanner sc=new Scanner(System.in);
+    static int n=5;
+	public static void main(String[] args) {
+	    int opcion;
+		do{
+		    System.out.println("\n=====Menu de figuras=====");
+		    System.out.println("1. Figura 1");
+		    System.out.println("2. Figura 2");
+		    System.out.println("3. Figura 3");
+		    System.out.println("4. Figura 4");
+		    System.out.println("5. Salir");
+		    
+		}while (opcion!=5);
+		
+	}
+	public static void figura1(int n){
+	    for (k,k<=n,k++){
+	        System.out.println(" " * (n-i) + "* " * i);
+	    }
+	public static void menu(){
+	    System.out.println("\n=====Menu de figuras=====");
+	    System.out.println("1. Figura 1");
+	    System.out.println("2. Figura 2");
+	    System.out.println("3. Figura 3");
+	    System.out.println("4. Figura 4");
+	    System.out.println("5. Salir");
+	}
+	}
+}
